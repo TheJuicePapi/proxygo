fab9f4c5958f86a496d07eb629a22e40c36ab53b
TheJuicePapi <134894632+TheJuicePapi@users.noreply.github.com>
2023-05-28 20:29:20 -0400

    Add files via upload

---
+++ proxygo.py
@@ -0,0 +1,62 @@
+             ### This py script is my first project and will be used to automate tor services and then launching proxychains right after. ### 
+
+print("Hello Juice Papi")
+
+import time 
+
+time.sleep(1.5) # Wait for 1.5 seconds
+print("starting tor services...")
+
+import subprocess
+
+subprocess.run(["sudo","systemctl","start","tor"])
+
+import time 
+
+time.sleep(1.5) # Wait for 1.5 seconds
+print("done!")
+
+import time
+import subprocess
+
+time.sleep(2) # Wait for 2 seconds
+
+# Get the status of tor service
+result = subprocess.run(["sudo","systemctl","status","tor"], capture_output=True)
+
+# Print the output of the command
+print(result.stdout.decode())
+
+import time 
+
+time.sleep(1) # Wait for 1 seconds
+print("Now starting proxychains for you...")
+
+
+
+
+import subprocess
+import webbrowser
+
+# Define the URL
+url = "www.dnsleaktest.com"
+
+# Run the proxychains command to open Firefox with the website
+firefox_process = subprocess.Popen(["proxychains","firefox",url])
+
+# Wait for firefox to exit
+firefox_process.wait()
+
+
+# Tor stop
+subprocess.run(["sudo","systemctl","stop","tor"])
+
+import time 
+
+time.sleep(1) # Wait for 1 seconds
+print("Firefox has exited and tor services have been disabled...")
+
+
+
+
+
