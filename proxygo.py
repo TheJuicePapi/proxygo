6d898863d2ae5b1314b62c893e4615a79a9cc086
TheJuicePapi <134894632+TheJuicePapi@users.noreply.github.com>
2023-06-01 19:46:32 -0400

    proxygo.py

--- proxygo.py
+++ proxygo.py
@@ -1,34 +1,44 @@
 
 art = """
 
- ____                       ____
-|  _ \ _ __ _____  ___   _ / ___| ____
-| |_) | '__/ _ \ \/ / | | | |  _ / _  }
-|  __/| | | (_) >  <| |_| | |_| | (_) |
-|_|   |_|  \___/_/\_\__,  |\____|\____}
-                     |___/
+
+                   *    *      *    *     *     *       *
+           *   *      *    *      *          *       *     *
+                *  ____        *     *    *   ____ *   *      *
+            *     |  _ \ _ __ _____  ___   _ / ___| ____    *
+         *     *  | |_) | '__/ _ \ \/ / | | | |  _ / _  }      *
+           *      |  __/| | | (_) >  <| |_| | |_| | (_) |   *
+             *    |_|   |_|  \___/_/\_\__,  |\____|\____} *    *
+          *     *           *          |___/
+                       *        *                 *       *    *
+           *   *    *    *   *    *     *      *     *      *
+                 *    *        *      *     *     *       *
 """
 
 print(art)
 
 
 
-import time 
+import time
+import subprocess
+import webbrowser
 
 time.sleep(1.5) # Wait for 1.5 seconds
-print("starting tor services...")
 
-import subprocess
+print("                  +=====================================+")
+print("                 [||                                   ||]")
+print("          +===[||[||  Starting TOR services for you... ||]||]===+")
+print("                 [||                                   ||]")
+print("                  +=====================================+")
+
 
 subprocess.run(["sudo","systemctl","start","tor"])
 
-import time 
 
 time.sleep(1.5) # Wait for 1.5 seconds
-print("done!")
-
-import time
-import subprocess
+print("                                          ")
+print("                                [  DONE! ]")
+print("                                          ")
 
 time.sleep(2) # Wait for 2 seconds
 
@@ -38,16 +48,14 @@ result = subprocess.run(["sudo","systemctl","status","tor"], capture_output=True
 # Print the output of the command
 print(result.stdout.decode())
 
-import time 
 
+time.sleep(.5) # Wait for .5 seconds
+print("                                                                        ")
+print("     * ============================================================== * ")
+print("     *              Now starting PROXYCHAINS for you...               * ")
+print("     * ============================================================== * ")
+print("                                                                        ")
 time.sleep(1) # Wait for 1 seconds
-print("Now starting proxychains for you...")
-
-
-
-
-import subprocess
-import webbrowser
 
 # Define the URL
 url = "www.dnsleaktest.com"
@@ -62,24 +70,24 @@ firefox_process.wait()
 # Tor stop
 subprocess.run(["sudo","systemctl","stop","tor"])
 
-import time 
 
 time.sleep(1) # Wait for 1 seconds
-print("   *** Firefox session has ENDED and Tor services have been DISABLED ***")
-
+print("                                                                         ")
+print("                                                                         ")
+print("     * ============================================================== *  ")
+print("   * * Firefox session has ENDED and Tor services have been DISABLED  * *")
+print("     * ============================================================== *  ")
 
 art = """
 
- _____ _              _          __                   _           _
-|_   _| |_  __ _ _ _ | |__ ___  / _|___ _ _   _  _ __(_)_ _  __ _| |
-  | | | ' \/ _` | ' \| / /(_-< |  _/ _ \ '_| | || (_-< | ' \/ _` |_|
-  |_| |_||_\__,_|_||_|_\_\/__/ |_| \___/_|    \_,_/__/_|_||_\__, (_)
-                                                            |___/
+ +========================================================================+
+ ||  _____ _              _          __                   _           _  ||
+ || |_   _| |_  __ _ _ _ | |__ ___  / _|___ _ _   _  _ __(_)_ _  __ _| | ||
+ ||   | | | ' \/ _` | ' \| / /(_-< |  _/ _ \ '_| | || (_-< | ' \/ _` |_| ||
+ ||   |_| |_||_\__,_|_||_|_\_\/__/ |_| \___/_|    \_,_/__/_|_||_\__, (_) ||
+ ||                                                              |___/   ||
+ +========================================================================+
+
 """
 
 print(art)
-
-
-
-
-
